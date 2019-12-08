d2dc617c29aa57e93940ed6445b11494db4cba2f
Jayanta Biswas <bjayanta.me@gmail.com>
2019-12-08 17:49:30 +0600

    Python syllabus.

--- RelationalOperator.py
+++
@@ -1,8 +0,0 @@
-
-print(30 > 20) # True
-print(30 < 20) # False
-print(30 >= 20) # True
-print(30 <= 20) # False
-print(30 == 20) # False
-print(30 != 20) # True
-print("Jayanta" == "Jayanta") # True
--- backslash_characters.py
+++
@@ -1,9 +0,0 @@
-# By new line
-print("Hello World! \n Hi! There.")
-
-# Tab
-print("Hello World! \t Hi! There.")
-
-# Double quote
-print("\"Jayanta Biswas\"")
-print("\'Python\' and \'Java\'")
