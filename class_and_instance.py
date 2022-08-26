bae67afdfe1d1479995114ae3affaa3e4701df9e
bjayanta <bjayanta.me@gmail.com>
2022-08-26 21:16:42 +0600

    section 1 is done

--- class_and_instance.py
+++ class_and_instance.py
@@ -5,8 +5,32 @@ se2 = ["Software Engineer", "Lisa", 25, "Senior", 7000]
 
 # class
 class SoftwareEngineer:
-    pass
+
+    # class attributes
+    alias = "Keyboard magician"
+    
+    def __init__(self, name, age, level, salary):
+        # instance attributes
+        self.name = name;
+        self.age = age;
+        self.level = level;
+        self.salary = salary;
 
 
 # instance
-se1 = SoftwareEngineer() 
+se1 = SoftwareEngineer("Max", 20, "Junior", 5000) 
+print(se1.name, se1.age)
+# print(SoftwareEngineer.name) # error
+print(se1.alias)
+print(SoftwareEngineer.alias)
+
+se2 = SoftwareEngineer("Lisa", 25, "Senior", 7000)
+print(se2.alias)
+print(SoftwareEngineer.alias)
+
+# Recap
+# create a class (blueprint)
+# create a instance (object)
+# class vs instance 
+# instance attributes: defined in __init__(self)
+# class attributes
