4336d696ea28a56d34e08a30110927951fbe4b87
Jayanta <bjayanta.me@gmail.com>
2022-08-26 13:24:51 +0600

    work with class & instance

---
+++ class_and_instance.py
@@ -0,0 +1,12 @@
+
+# position, name, age, level, salary
+se1 = ["Software Engineer", "Max", 20, "Junior", 5000]
+se2 = ["Software Engineer", "Lisa", 25, "Senior", 7000]
+
+# class
+class SoftwareEngineer:
+    pass
+
+
+# instance
+se1 = SoftwareEngineer() 
